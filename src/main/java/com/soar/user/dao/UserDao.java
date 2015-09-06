0a578a8e5ff8d1bd107e5324b4cb6dc02c2b3506
soaryang <759620299@qq.com>
2015-09-06 14:16:43 +0800

    add a method to userDao

--- src/main/java/com/soar/user/dao/UserDao.java
+++ src/main/java/com/soar/user/dao/UserDao.java
@@ -4,4 +4,6 @@ package com.soar.user.dao;
  * Created by user on 15/9/6.
  */
 public interface UserDao {
+
+    void add();
 }
--- src/main/java/com/soar/user/dao/UserDaoImpl.java
+++ src/main/java/com/soar/user/dao/UserDaoImpl.java
@@ -4,4 +4,8 @@ package com.soar.user.dao;
  * Created by user on 15/9/6.
  */
 public class UserDaoImpl implements UserDao{
+    @Override
+    public void add() {
+
+    }
 }
