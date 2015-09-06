0856e33f9671c11047989a8b7257fb82e65e7adf
soaryang <759620299@qq.com>
2015-09-07 00:27:48 +0800

    代码修改

--- src/main/java/com/soar/common/myReturnValue/FirstResultValueHander.java
+++ src/main/java/com/soar/common/myReturnValue/FirstResultValueHander.java
@@ -9,13 +9,12 @@ import org.springframework.web.method.support.ModelAndViewContainer;
  * Created by Administrator on 2015/8/30 0030.
  */
 public class FirstResultValueHander implements HandlerMethodReturnValueHandler {
-    @Override
+
     public boolean supportsReturnType(MethodParameter methodParameter) {
         System.out.println(")))))))))))))))))))))))))))))))))))))))))))))begin");
         return true;
     }
 
-    @Override
     public void handleReturnValue(Object o, MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest) throws Exception {
         System.out.print("---------------------------------------myResult");
     }
