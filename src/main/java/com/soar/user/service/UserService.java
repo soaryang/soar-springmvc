11efd08d60df52427c6de311ccf9a156c3901333
soaryang <759620299@qq.com>
2015-09-06 15:14:44 +0800

    Merge branch 'develop'
    # Conflicts:
    #	src/main/java/com/soar/user/dao/UserDao.java
    #	src/main/java/com/soar/user/dao/UserDaoImpl.java

--- src/main/java/com/soar/user/service/UserService.java
+++ src/main/java/com/soar/user/service/UserService.java
@@ -4,4 +4,6 @@ package com.soar.user.service;
  * Created by user on 15/9/6.
  */
 public interface UserService {
+
+    void delete();
 }
--- src/main/java/com/soar/user/service/UserServiceImpl.java
+++ src/main/java/com/soar/user/service/UserServiceImpl.java
@@ -4,4 +4,9 @@ package com.soar.user.service;
  * Created by user on 15/9/6.
  */
 public class UserServiceImpl implements UserService{
+
+    @Override
+    public void delete() {
+
+    }
 }
